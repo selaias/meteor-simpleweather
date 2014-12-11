6cb69a5576dd83c15d7d3c693fe1185ffa10da38
selaias <s.elaias@gmail.com>
2014-12-11 19:05:39 +0000

    fixing dependencies

--- package.js
+++ package.js
@@ -1,27 +1,35 @@
 Package.describe({
   name: 'selaias:meteor-simpleweather',
   summary: ' Wrapper for simpleWeather api for jQuery (http://simpleweatherjs.com/) ',
-  version: '0.4.0',
+  version: '0.6.2',
   git: ' https://github.com/selaias/meteor-simpleweather.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.0");
-  api.use('jquery');
-  
+  api.versionsFrom('0.9.0');
+  api.use([
+    'templating',
+    'handlebars',
+    'jquery',
+    'json'
+  ], 'client');
+
   var path = Npm.require('path');
   var asset_path = path.join('simpleweather');
   //css
   api.addFiles(path.join(asset_path, 'css', 'simpleweather.css'), 'client');
-  
+
   //js
   api.addFiles(path.join(asset_path, 'js', 'simpleweather.js'), 'client');
-  
+
   // fonts
   api.addFiles(path.join(asset_path, 'fonts', 'artill_clean_icons.otf'), 'client');
-  
+
   //template
   api.addFiles("weather.html", 'client');
   api.addFiles("weather.js", 'client');
-  
+  if(api.export) {
+    api.export('Weather', 'client')
+  }
+
 });
